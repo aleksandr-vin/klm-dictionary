429267ed9b13b76171073aad94d555ba7bec4b0a
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2022-04-21 15:50:47 +0200

    fix: install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ source .env
 
 echo "Installing into ${DEST_PATH}".
 mkdir -p "${DEST_PATH}"
-ditto --noextattr --norsrc "objects/${DICT_NAME}.dictionary" "${DEST_PATH}/${DICT_NAME}.dictionary"
+ditto --noextattr --norsrc "objects/target/${DICT_NAME}.dictionary" "${DEST_PATH}/${DICT_NAME}.dictionary"
 touch "${DEST_PATH}"
 echo "Done."
 echo "To test the new dictionary, open Dictionary.app, then open Preferences and enable ${DICT_NAME} dictionary"
